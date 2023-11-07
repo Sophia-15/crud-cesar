94e4010c5c3e8cead0deea54654e8759e91ec22f
Gustavo Mourato <gmam@cesar.school>
2023-11-07 18:33:28 -0300

    as

--- adicionar.py
+++ adicionar.py
@@ -1,6 +1,8 @@
 import os
 os.system("cls")
 biblioteca = {}
+categorias = []
+gastosvalores = []
 def adicionar ():
     numlivro = input("Digite o número para cadastrar o livro: ")
     biblioteca[numlivro] = []
@@ -8,9 +10,13 @@ def adicionar ():
     autor = input("Agora insira as informações do livro abaixo, começando pelo autor do livro: ")
     genero = input("Gênero: ")
     dinheiro = input("Preço: ")
+    nota = input("Nota pessoal: ")
     biblioteca[numlivro].append(nome)
     biblioteca[numlivro].append(autor)
     biblioteca[numlivro].append(genero)
     biblioteca[numlivro].append(dinheiro)
+    biblioteca[numlivro].append(nota)
+    categorias.append(genero)
+    gastosvalores.append(dinheiro)
     return biblioteca
 print(adicionar())
