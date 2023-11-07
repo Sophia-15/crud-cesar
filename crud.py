63a5764b470f0a6e840bdbc5621d1e0a2d79a514
Paulo Rosado <paulohenrique110803@gmail.com>
2023-11-07 18:11:13 -0300

    menu

--- crud.py
+++ crud.py
@@ -1,1 +1,26 @@
+import os
+os.system('cls')
+
 biblioteca = {}
+
+def menu():
+    print(f"📚 Olá Nathália! Bem-vinda ao Sistema de Gerenciamento de Leitura (SGL)")
+    acao = int(input("[1] Visualizar a sua lista de livros\n[2] Adicionar um novo livro\n[3] Editar as informações de um livro\n[4] Excluir um livro\n[5] Sair\nDigite o número correspondente: "))
+    if acao == 1:
+        visualizar()
+    elif acao == 2:
+        adicionar()
+    elif acao == 3:
+        editar()
+    elif acao == 4:
+        excluir()
+    elif acao == 5:
+        return 5
+    else:
+        print('CÓDIGO INVÁLIDO')
+
+while True:
+    menu()
+    saida = menu()
+    if saida == 5:
+        break
