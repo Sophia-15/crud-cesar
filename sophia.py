3380665d0d70dc639d96258b4a416dd8d52bcda2
sophia <deletrasopa@gmail.com>
2023-11-07 18:50:41 -0300

    adicionando função de filtro

--- sophia.py
+++ sophia.py
@@ -1,5 +1,7 @@
 biblioteca = {
-  1: ['Berserk', 'Kentaro Miura', 'Seinen', 24.9],
+  'berserk': ['Berserk', 'Kentaro Miura', 'Seinen', 24.9],
+  'cu': ['Berserk', 'Kentaro Miura', 'Seinen', 24.9],
+  'eu': ['Berserk', 'Kentaro Miura', 'klasjdfk', 24.9],
 }
 
 def visualizar_livros():
@@ -9,5 +11,14 @@ def visualizar_livro(livro):
   livro = biblioteca.get(livro, 'Livro não encontrado')
   print(livro)
 
-def excluir_livro(livro):
+def excluir_livro():
+  visualizar_livros()
+  livro = int(input("Digite o número do livro que deseja excluir: "))
   print(biblioteca.pop(livro, 'Livro não encontrado'))
+
+def filtrar_categoria(valor_categoria):
+  for livro in biblioteca.items():
+    for categoria in livro[1]:
+      if categoria == valor_categoria:
+        print(livro[1])
+
