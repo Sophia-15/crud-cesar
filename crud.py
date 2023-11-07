7660f4038928f10c3a372306c579548ec3300228
sophia <deletrasopa@gmail.com>
2023-11-07 17:46:45 -0300

    adicionando visualização e deleção

--- crud.py
+++ crud.py
@@ -1,1 +1,13 @@
-biblioteca = {}
+biblioteca = {
+  1: ['Berserk', 'Kentaro Miura', 'Seinen', 24.9],
+}
+
+def visualizar_livros():
+  print(biblioteca)
+
+def visualizar_livro(livro):
+  livro = biblioteca.get(livro, 'Livro não encontrado')
+  print(livro)
+
+def excluir_livro(livro):
+  print(biblioteca.pop(livro, 'Livro não encontrado'))
