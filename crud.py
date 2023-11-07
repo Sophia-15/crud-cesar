0ef5102ca775a87a5edd978d9536ec18855dab4a
Gustavo Mourato <gmam@cesar.school>
2023-11-07 18:13:00 -0300

    funcao adicao

--- crud.py
+++ crud.py
@@ -1,1 +1,19 @@
+import os
+os.system("cls")
+
 biblioteca = {}
+
+def adicionar ():
+    numlivro = input("Digite o número para cadastrar o livro: ")
+    biblioteca[numlivro] = []
+    nome = input("Insira o nome do livro que você deseja adicionar: ")
+    autor = input("Agora insira as informações do livro abaixo, começando pelo autor do livro: ")
+    genero = input("Gênero: ")
+    dinheiro = input("Preço: ")
+    biblioteca[numlivro].append(nome)
+    biblioteca[numlivro].append(autor)
+    biblioteca[numlivro].append(genero)
+    biblioteca[numlivro].append(dinheiro)
+    return biblioteca
+
+print(adicionar())
