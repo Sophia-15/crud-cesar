8b69e0f64d0df90b98a5668a7555622877036e78
Gustavo Mourato <gmam@cesar.school>
2023-11-08 00:21:06 -0300

    condicao de nota adicioanda de volta; avaliacao de livros nao lidos adicionadas

--- adicionar.py
+++ adicionar.py
@@ -11,12 +11,15 @@ def adicionar ():
     autor = input("Agora insira as informações do livro abaixo, começando pelo autor do livro: ")
     genero = input("Gênero: ")
     dinheiro = input("Preço: ")
-    nota = input("Nota pessoal (0 a 5): ")
+    nota = int(input("Nota pessoal (0 a 5, digite 6 se for um livro que você não leu ainda): "))
 
     biblioteca[nome].append(autor)
     biblioteca[nome].append(genero)
     biblioteca[nome].append(dinheiro)
-    biblioteca[nome].append(nota)
+    if nota >= 0 and nota <= 5:
+        biblioteca[nome].append(nota)
+    elif nota == 6:
+        biblioteca[nome].append("Não avaliado")
     categorias.append(genero)
     gastosvalores.append(dinheiro)
     
